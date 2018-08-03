01375c5130ee9b1d3ae0724859348bf57d8343df
Denis Zakharnev <denis.zakharnev@gmail.com>
2018-08-04 02:53:06 +0400

    loadGameRecords

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -3,6 +3,7 @@ package controller;
 import domain.GameRecords;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,12 @@ public class GameController {
 
     @RequestMapping(value = "/game/{gameId}", method = RequestMethod.GET)
     public ResponseEntity<?> loadGameRecords(@PathVariable("gameId") Long gameId) {
-        GameRecords gameRecords = gameService.loadGameRecords(gameId);
+        try {
+            GameRecords gameRecords = gameService.loadGameRecords(gameId);
 
-        return ResponseEntity.ok(gameRecords);
+            return ResponseEntity.ok(gameRecords);
+        } catch  (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -2,6 +2,7 @@ package service;
 
 import domain.GameRecords;
 import domain.Move;
+import lombok.extern.slf4j.Slf4j;
 import mybatis.GameMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,17 +10,23 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Slf4j
 public class GameService {
 
     @Autowired
     private GameMapper gameMapper;
 
     public GameRecords loadGameRecords(Long gameId) {
-        GameRecords gameRecords = gameMapper.loadGameRecords(gameId);
+        try {
+            GameRecords gameRecords = gameMapper.loadGameRecords(gameId);
 
-        List<Move> moves = gameMapper.loadGameMoves(gameId);
-        gameRecords.setMoves(moves);
+            List<Move> moves = gameMapper.loadGameMoves(gameId);
+            gameRecords.setMoves(moves);
 
-        return gameRecords;
+            return gameRecords;
+        } catch (Exception e) {
+            log.error("Error while loading records for game id {}", gameId, e);
+            throw new RuntimeException("Error while loading records for game id " + gameId);
+        }
     }
 }
