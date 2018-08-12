0ff2f02f493f30fa14ee34aac4c1678da3a9494d
Denis Zakharnev <denis.zakharnev@gmail.com>
2018-08-12 23:46:36 +0300

    fix test

--- src/main/lombok/domain/Country.java
+++ src/main/lombok/domain/Country.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Country {
-    private String id;
+    private Long id;
     private String name;
     private String shortName;
 }
--- src/test/java/controller/DictionaryControllerTest.java
+++ src/test/java/controller/DictionaryControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,14 +17,33 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(SpringRunner.class)
-@ActiveProfiles("dev")
 @SpringBootTest(classes = ChesspediaApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DictionaryControllerTest {
 
     public static final long PLAYER_ID = 1L;
 
+    public static final long COUNTRY_ID = 2L;
+    public static final String COUNTRY_CODE = "NOR";
+
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Test
+    public void testCountryById() {
+        ResponseEntity<Country> response = restTemplate.getForEntity(
+                "/dictionary/country/" + COUNTRY_CODE,
+                Country.class
+        );
+
+        assertThat(response, notNullValue());
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody(), notNullValue());
+
+        Country country = response.getBody();
+        assertThat(country.getId(), is(COUNTRY_ID));
+        assertThat(country.getName(), is("Norway"));
+        assertThat(country.getShortName(), is("NOR"));
+    }
 
     @Test
     public void testFindPlayerById() {
@@ -44,6 +62,7 @@ public class DictionaryControllerTest {
 
         assertThat(player.getCountry(), notNullValue());
         Country country = player.getCountry();
+        assertThat(country.getId(), is(COUNTRY_ID));
         assertThat(country.getName(), is("Norway"));
         assertThat(country.getShortName(), is("NOR"));
     }
